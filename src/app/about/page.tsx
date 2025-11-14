acb569eb6503a8dabb9dc4d21547add1da49273f
Chris Woodby <chrisdwoodby@gmail.com>
2025-11-14 02:32:17 +0200

    about page animation staged

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 export default function Page() {
     useEffect(() => {
         window.addEventListener('scroll', function() {
+          const pageBody = document.querySelector('.about-me-page') as HTMLElement | null;
           const pillers = document.querySelector('.about-page-hero') as HTMLElement | null;
           const element = document.querySelector('.acomplishments') as HTMLElement | null;
           if (element) {
@@ -13,6 +14,7 @@ export default function Page() {
                   // Partial visibility code can go here
                   pillers.classList.add("acomplishments-in-view");
                   pillers.classList.remove("acomplishments-not-in-view");
+                  pageBody.classList.add("animated");
             }
           }
           if (pillers) {
@@ -21,23 +23,24 @@ export default function Page() {
           if (position.top >= 0 && position.bottom <= window.innerHeight) {
                 pillers.classList.add("acomplishments-not-in-view");
                 pillers.classList.remove("acomplishments-in-view");
+                pageBody.classList.remove("animated");
           }
         }
         });
       });
     return (
-        <div>
+        <div className="about-me-page">
             <div className="about-page-hero">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-6">
+                        <div className="">
                             <h1>About me</h1>
                             <span>Developer | Designer</span>
 
                             <div className="piller-group">
-                                <div className="piller-image"></div>
-                                <div className="piller-image"></div>
-                                <div className="piller-image"></div>
+                                <div className="piller-wide"></div>
+                                <div className="piller-2"></div>
+                                <div className="piller-3"></div>
                             </div>
 
                         </div>
@@ -46,7 +49,24 @@ export default function Page() {
                 </div>
             </div>
             <div className="acomplishments" role="complementary">
-
+                <div className="container">
+                    <h2 className="acomplishments-heading">Who i've helped</h2>
+                </div>
+            </div>
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <span>787 Collective</span>
+                        </div>
+                        <div className="col-md-4">
+                            <span>Gather Austin</span>
+                        </div>
+                        <div className="col-md-4">
+                            <span>Austin Seminary</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )

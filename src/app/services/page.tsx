06c5a3e41cbf88d40053c36dccac0b68f8f08de7
Chris Woodby <chrisdwoodby@gmail.com>
2025-11-19 04:02:50 +0200

    services page hero text animation added

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,26 +1,42 @@
+"use client"; 
 import Navbar from '@/app/navbar';
+import { useState, useEffect } from 'react';
 export default function Page() {
-  window.addEventListener("load", (event) => {
-    let serviceList = ['SEO', 'Accessibility', 'Custom Development', 'Hosting'];
-    const cycleList = function() {
-      serviceList.forEach((item) => {
-        console.log(item);
-        document.querySelector('.hero-text-cycle').innerHTML = 'hero text';
-        
-      })
-      
-    }
-    setInterval(() => {
-      cycleList();
-    }, 2);
-  });
+  let serviceList = ['SEO', 'Accessibility', 'Custom Development', 'Hosting'];
+  let index = 0;
+  useEffect(() => {
+    const heroText = document.querySelector('.hero-text-cycle') as HTMLElement | null;
+    if (!heroText) return; 
+    heroText.classList.add('text-fade'); 
 
+    const changeText = () => {
+
+      heroText.classList.add('text-hidden');
+
+      setTimeout(() => {
+        if (index === 3) {
+          index = 0;
+        }
+        heroText.innerHTML = serviceList[index];
+        index ++;
+  
+        heroText.classList.remove('text-hidden'); 
+      }, 600);
+    }
+    changeText();
+
+    const interval = setInterval(changeText, 4000);
+  
+    return () => clearInterval(interval);
+
+  }, []); 
     return (
       <div>
           <div className="services-hero">
               <div className="container fade-in-bottom">
                 <h1>Your Full Service Studio</h1>
                 <h3>Experts in <span className="hero-text-cycle">SEO</span></h3>
+                <a className="company-cta">Learn More</a>
               </div>
               <div className="searchbar fade-out">
                 <span>www.Chriswoodby.com</span>
